ebc3b88cc68c0c7b8caee1fdf6241f64c72f2915
Maryia Miller <maryiamiller@gmail.com>
2019-06-27 12:43:02 -0400

    feat(data): generted the key for the db, started on the seed script

--- datascraping/index.js
+++ datascraping/index.js
@@ -1,44 +1,10 @@
-const axios = require('axios');
-const jsonfile = require('jsonfile');
-// const url = 'https://brooklynbrewery.com/brooklyn-beers/perennial-brews';
-// const beerParse = require('./beerParse');
+const admin = require('firebase-admin');
 
-async function beerScrape(url) {
-  try {
-    let { data } = await axios.get(url);
-    for (let key in data){
-      beer.findOrCreat()
-    }
+const serviceAccount = require('../.serviceAcctKey.json');
 
-    console.log('data', data);
-  } catch (error) {
-    console.log('we have a problem', error);
-  }
-}
+const data = require('./data');
 
-// axios
-//   .get(url)
-//   //   .then(res => res.data)
-//   .then(html => {
-//     console.log(html);
-//     // const beerUrls = [];
-//     // for (let i = 0; i < 45; i++) {
-//     //   beerUrls.push($('big > a', html)[i].attribs.href);
-//     // }
-//     // console.log('beerUrls', beerUrls)
-//     // return Promise.all(
-//     //   wikiUrls.map(url => {
-//     //     return beerParse('https://en.wikipedia.org' + url);
-//     //   })
-//     // );
-//   })
-//   //   .then(function(presidents) {
-//   //     console.log(presidents);
-//   //   })
-//   .catch(err) {
-//     console.log('got error')
-//   });
-
-beerScrape(
-  `https://sandbox-api.brewerydb.com/v2/beers/?p=1&key=f2bea26fda6949376193a495216ceab8`
-);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://beer-f6545.firebaseio.com',
+});
