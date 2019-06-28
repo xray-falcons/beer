983d874a16e16b97fc930ea450d878114201a44b
Jing Cao <jingcao.me@gmail.com>
2019-06-28 17:17:13 -0400

    feat(singlebeer) refactored to functional component

--- components/SingleBeer.js
+++ components/SingleBeer.js
@@ -4,52 +4,32 @@ import firebase from "firebase";
 import connect from "react-redux";
 import { db } from "../server/db";
 
-const beerImage =
-  "https://brewerydb-images.s3.amazonaws.com/beer/QJZFnY/upload_h8SNgF-contentAwareMedium.png";
+//const beerImage =
+//  "https://brewerydb-images.s3.amazonaws.com/beer/QJZFnY/upload_h8SNgF-contentAwareMedium.png"; 
 
-export default class SingleBeer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+// will refactor so that beer info is passed in props
+// will refactor again so that beer info is passed from redux store
+
+const props = {
       beerName: "myname",
-      beerImage: "",
+      beerImage: "https://brewerydb-images.s3.amazonaws.com/beer/QJZFnY/upload_h8SNgF-contentAwareMedium.png",
       abv: 10,
       description: "hello",
       ibu: 20,
       brewery: "mybrewery",
       style: "mystyle"
     };
-  }
 
-  query = async () => {
-    try {
-      console.log("single beer query");
-      const beers = await db.collection("beers");
-      const query = await beers.doc(1);
-      console.log(query);
-      // let arr = [];
-      // query.get().then(function(querySnapshot) {
-      //   querySnapshot.forEach(function(doc) {
-      //     let beer = doc.data();
-      //     arr.push(beer);
-      //   });
 
-      // console.log(arr);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  render() {
+export const SingleBeer = props => {
     return (
       <View style={styles.container}>
-        <Text style={styles.headText}>{this.state.beerName}</Text>
-        <Image source={{ uri: beerImage }} style={styles.image} />
-        <Text style={styles.text}>abv:{this.state.abv}</Text>
-        <Text style={styles.text}>{this.state.description}</Text>
+        <Text style={styles.headText}>{props.beerName}</Text>
+        <Image source={{ uri: props.beerImage }} style={styles.image} />
+        <Text style={styles.text}>abv:{props.abv}</Text>
+        <Text style={styles.text}>{props.description}</Text>
       </View>
     );
-  }
 }
 
 const styles = StyleSheet.create({
