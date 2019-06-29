607e3dfecdc47ef700d983721968a0375c7f1807
Nick B <nicholas.balestrieri@gmail.com>
2019-06-29 12:48:06 -0400

    fixed Icon prop names

--- components/SingleBeer.js
+++ components/SingleBeer.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { View, Text, ActivityIndicator, StyleSheet, Image } from "react-native";
-import firebase from "firebase";
-import connect from "react-redux";
-import { db } from "../server/db";
+import Icon from "react-native-vector-icons/FontAwesome";
+const thumbsUp = <Icon name="thumbsup" size={20} color="#005"/>
+const thumbsDown = <Icon name="thumbsdown" size={20} color="#005"/>
+
+// import firebase from "firebase";
+// import connect from "react-redux";
+// import { db } from "../server/db";
 
 //const beerImage =
-//  "https://brewerydb-images.s3.amazonaws.com/beer/QJZFnY/upload_h8SNgF-contentAwareMedium.png"; 
+//  "https://brewerydb-images.s3.amazonaws.com/beer/QJZFnY/upload_h8SNgF-contentAwareMedium.png";
 
 // will refactor so that beer info is passed in props
 // will refactor again so that beer info is passed from redux store
@@ -20,35 +24,41 @@ const props = {
       style: "mystyle"
     };
 
+    const styles = StyleSheet.create({
+      container: {
+        flex: 0.6,
+        justifyContent: "center",
+        alignItems: "center"
+      },
+      image: {
+        width: 200,
+        height: 200,
+        marginTop: 20,
+        marginBottom: 20,
+        borderRadius: 100
+      },
+      headText: {
+        fontSize: 48
+      },
+      text: {
+        fontSize: 20
+      }
+    });
 
-export const SingleBeer = props => {
+const SingleBeer = () => {
     return (
       <View style={styles.container}>
         <Text style={styles.headText}>{props.beerName}</Text>
         <Image source={{ uri: props.beerImage }} style={styles.image} />
         <Text style={styles.text}>abv:{props.abv}</Text>
         <Text style={styles.text}>{props.description}</Text>
+        <Icon.Button name="thumbs-up" size={20} color="#005"/>
+        <Icon.Button name="thumbs-down" size={20} color="#005"/>
       </View>
     );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 0.6,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  image: {
-    width: 200,
-    height: 200,
-    marginTop: 20,
-    marginBottom: 20,
-    borderRadius: 100
-  },
-  headText: {
-    fontSize: 48
-  },
-  text: {
-    fontSize: 20
-  }
-});
+
+export default SingleBeer
+
+
