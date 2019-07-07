2aeb6f5e8931408a6637afc58bfe8ad85aa8df5c
Maria Obaturova <m.obaturova@gmail.com>
2019-07-07 17:15:46 -0400

    feat(markers): added markers

--- components/map.js
+++ components/map.js
@@ -3,8 +3,8 @@ import { View, ScrollView, Text, Platform } from 'react-native';
 import { Button } from "react-native-elements"
 import { LinearGradient } from "expo-linear-gradient";
 import styles from "../style/styles";
-import MapView, { Marker,  } from 'react-native-maps';
-
+import MapView, { Marker } from 'react-native-maps';
+import YelpService from "../services/index";
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
@@ -13,20 +13,33 @@ const deltas = {
     longitudeDelta: 0.0091
 };
 
+
 export default class Map extends Component {
     constructor(props) {
         super()
         this.state = {
             region: null,
             errorMessage: null,
+            bars: []
         };
     }
     componentWillMount() {
-        this._getLocationAsync();
+        this.getLocationAsync();
 
     }
+    renderMarkers() {
+        return this.state.bars.map((place, i) => (
+            <Marker key={i} title={place.name} coordinate={place.coords} />
+        ))
+    }
+    getBars = async () => {
+        const { latitude, longitude } = this.state.region;
+        const userLocation = { latitude, longitude };
+        const bars = await YelpService.getBars(userLocation);
+        this.setState({ bars });
+    };
 
-    _getLocationAsync = async () => {
+    getLocationAsync = async () => {
         let { status } = await Permissions.askAsync(Permissions.LOCATION);
         if (status !== 'granted') {
             this.setState({
@@ -40,6 +53,8 @@ export default class Map extends Component {
             ...deltas
         };
         await this.setState({ region });
+        await this.getBars();
+
     };
 
     static navigationOptions = {
@@ -57,7 +72,8 @@ export default class Map extends Component {
                 showsMyLocationButton={false}
                 zoomEnabled = {true}
 
-            />
+            >{this.renderMarkers()}
+            </MapView>
         );
     }
 }
