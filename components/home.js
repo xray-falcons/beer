af0b779c0464f3eb666905ca5bf021af68a302d5
Maryia Miller <maryiamiller@gmail.com>
2019-07-09 17:08:59 -0400

    fix(home) refactored home component

--- components/home.js
+++ components/home.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import {Button} from "react-native-elements"
 import {LinearGradient} from "expo-linear-gradient";
 import firebase from 'firebase';
-import { db } from "../server/db";
 import { ScrollView } from 'react-native-gesture-handler';
 import styles from '../style/styles';
-import Beer from "./beer"
 import Frequent from "./frequent-beers"
 import Recent from "./recent-beers"
+import Recommended from "./recommended-beers"
 
 export default class Home extends React.Component {
-
-    constructor(){
-        super()
-        this.state = {
-            recommendedBeers: [],
-            userId: ''
-        }
-    }
     static navigationOptions = {
         headerTransparent: true,
         headerTitle: "",
@@ -26,36 +16,6 @@ export default class Home extends React.Component {
             <Button type="solid" buttonStyle={styles.attentionButton} title='Logout' onPress={() => firebase.auth().signOut()}/>
         ),
     };
-
-    async componentWillMount () {
-        const userId = await firebase.auth().currentUser.uid
-        this.setState({userId: userId})
-        //this.getRecommendations()
-    }
-
-
-    // getRecommendations = async () => {
-    //     const recommendedBeers = []
-    //     try {
-    //         const beers = await db.collection('beers')
-    //         const userQuery = await db.doc(`users/${this.state.userId}`).get()
-    //         const preferences = userQuery.data().preferences.map(elem => elem.toLowerCase())
-    //         for (let i = 0; i < preferences.length; i++){
-    //             const beerQuery = beers
-    //                 .where('taste', 'array-contains', preferences[i])
-    //                 .limit(2)
-    //             const querySnapshot = await beerQuery.get()
-    //             querySnapshot.forEach(function(doc){
-    //                 let beer = doc.data()
-    //                 recommendedBeers.push(beer)
-    //             })
-    //         }
-    //         this.setState({recommendedBeers})
-    //     } catch(err) {
-    //         console.log(err)
-    //     }
-    // }
-
 
     render() {
         return (
@@ -65,16 +25,7 @@ export default class Home extends React.Component {
                         <ScrollView>
                             <Recent navigation={this.props.navigation} />
                             <Frequent navigation={this.props.navigation}/>
-                            <View style={{marginTop: 10, justifyContent: "space-between"}}>
-                                <Text style={styles.titleText}>Top picks for you: </Text>
-                                <View style={{justifyContent: "space-between", marginTop: 15}}>
-                                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
-                                    {this.state.recommendedBeers.length ? this.state.recommendedBeers.map((eachBeer, idx) =>
-                                        <Beer key={idx} beer={eachBeer} navigation={this.props.navigation} />) : <Text style={styles.text}>Like some beers to show here!</Text>}
-                                </ScrollView>
-                                <Button onPress={this.getRecommendations} title="Get New Recommendations" />
-                                </View>
-                            </View>
+                            <Recommended navigation={this.props.navigation} />
                         </ScrollView>
             </LinearGradient>
         );
