8007dc2677017ba60b227f11d42a08dfefd386aa
Nick B <nicholas.balestrieri@gmail.com>
2019-07-10 14:34:15 -0400

    fix (walkthrough) changed urls to refer to cropped screenshots

--- components/pages.js
+++ components/pages.js
@@ -11,35 +11,35 @@ export default [
       description: "Filter beers by a variety of styles from around the world.",
       backgroundColor: '#e9a709',
       image: 'https://raw.githubusercontent.com/malyavka/GroupProject.FullBlastAcademy/master/20190710_004841.jpg?token=AJFKIIWBBC4YCTS7IUAGGXK5F2UQ4',
-  imageBottom: "https://raw.githubusercontent.com/malyavka/GroupProject.FullBlastAcademy/master/20190709_210409.jpg?token=AJFKIIVWFR3DAKJPMW3SPR25F2RVQ"
+  imageBottom: "https://raw.githubusercontent.com/xray-falcons/beer/walkthrough/style/bottom_bar_browse.jpg"
   },
   {
       title: 'Browse by taste',
       description: "Filter beers by flavor. You can combine up to three flavors at once.",
       backgroundColor: '#ffce11',
       image: 'https://raw.githubusercontent.com/malyavka/GroupProject.FullBlastAcademy/master/20190710_004810.jpg?token=AJFKIIW5MN5ZDZYMZCIFWAK5F2UO2',
-      imageBottom: "https://raw.githubusercontent.com/malyavka/GroupProject.FullBlastAcademy/master/20190709_210409.jpg?token=AJFKIIVWFR3DAKJPMW3SPR25F2RVQ"
+      imageBottom: "https://raw.githubusercontent.com/xray-falcons/beer/walkthrough/style/bottom_bar_browse.jpg"
   },
   {
       title: 'Know the name?',
       description: "Perfect! Just put it in the search bar! And our smart engine will find it for you!",
       backgroundColor: '#d35400',
       image: 'https://raw.githubusercontent.com/malyavka/GroupProject.FullBlastAcademy/master/20190710_005142.jpg?token=AJFKIITTQQUCS7DQDPCHF4C5F2UZC',
-      imageBottom: "https://raw.githubusercontent.com/malyavka/GroupProject.FullBlastAcademy/master/20190709_210350.jpg?token=AJFKIIXA5ZFLVKY2SFSGBQC5F2RSW"
+      imageBottom: "https://raw.githubusercontent.com/xray-falcons/beer/walkthrough/style/bottom_bar_search.jpg"
   },
   {
       title: 'Still have no beer?',
       description: "Find a bar nearby if there’s no beer within arm's reach.",
       backgroundColor: '#863705',
       image: 'https://raw.githubusercontent.com/malyavka/GroupProject.FullBlastAcademy/master/20190710_004303.jpg?token=AJFKIIWRQTZXRAGVOGC7YYC5F2UAU',
-  imageBottom: "https://raw.githubusercontent.com/malyavka/GroupProject.FullBlastAcademy/master/20190709_210159.jpg?token=AJFKIISUB6JVYP3XFJG5ATK5F2RY6"
+  imageBottom: "https://raw.githubusercontent.com/xray-falcons/beer/walkthrough/style/bottom_bar_map.jpg"
   },
   {
       title: 'Feel Lucky?',
       description: "Truth or Dare, but without the Truth. Try a random beer!",
       backgroundColor: '#bc870c',
       image: 'https://raw.githubusercontent.com/malyavka/GroupProject.FullBlastAcademy/master/20190710_004316.jpg?token=AJFKIIWPW77CGBBVUMMOPXC5F2T62',
-      imageBottom: "https://raw.githubusercontent.com/malyavka/GroupProject.FullBlastAcademy/master/20190709_210121.jpg?token=AJFKIIUBAAYRS5WMCUKFF425F2ROY"
+      imageBottom: "https://raw.githubusercontent.com/xray-falcons/beer/walkthrough/style/bottom_bar_random.jpg"
 
   },
 
